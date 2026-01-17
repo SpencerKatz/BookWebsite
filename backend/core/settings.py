3ede705609a7178d2bae8377ef6b3adfa6ac6566
Judy He <judyhe@Judys-MacBook-Pro.local>
2026-01-17 14:58:17 -0500

    update folders

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
@@ -82,6 +84,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "bookwebsite",
+#         "USER": "postgres",
+#         "PASSWORD": "judy0119",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
